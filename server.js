19237cc649cf761cdf144e2334b4af696b800cd1
Kiseri  Habte <109975015+Kiserry21@users.noreply.github.com>
2022-11-10 23:20:00 -0500

    created function to grab employee from database

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var managers = [];
 var roles = [];
 var departments = [];
 var employees = [];
+
 // connection to mysql database
 const connection = mysql.createConnection({
   host: "localhost",
@@ -16,3 +17,25 @@ const connection = mysql.createConnection({
   password: "password",
   database: "employeesDB",
 });
+
+// function to insert employees into an array after retrieving them from the database
+const getEmployee = () => {
+    connection.query(
+      `SELECT first_name, last_name, id FROM employee`,
+      (err, res) => {
+        if (err) throw err;
+        employees = [];
+        for (let i = 0; i < res.length; i++) {
+          const id = res[i].id;
+          const firstName = res[i].first_name;
+          const lastName = res[i].last_name;
+          var newEmployees = {
+            name: firstName.concat(" ", lastName),
+            value: id,
+          };
+          employees.push(newEmployees);
+        }
+        return employees;
+      }
+    );
+  };
