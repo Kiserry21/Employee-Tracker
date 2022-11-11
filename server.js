1da345734880d2ed509b293c16818ae12102c62c
Kiseri  Habte <109975015+Kiserry21@users.noreply.github.com>
2022-11-10 23:09:55 -0500

    created arrays for my tables

--- server.js
+++ server.js
@@ -2,3 +2,17 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTables = require("console.table");
+
+// created blank arrays for my tables
+var managers = [];
+var roles = [];
+var departments = [];
+var employees = [];
+// connection to mysql database
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "employeesDB",
+});
