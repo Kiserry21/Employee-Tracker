126f52b6d00092161ea1d74c72879e72ee1b1304
Kiseri  Habte <109975015+Kiserry21@users.noreply.github.com>
2022-11-14 22:57:18 -0500

    replaced the functions to inquirer prompt

--- server.js
+++ server.js
@@ -31,11 +31,11 @@ function menu() {
             "add a role", 
             "update an employee role", 
             "add employee", 
-            "view all employees by manager",
-            "remove departtment",
+            "view all employee by manager",
+            "remove department",
             "remove employee",
             "update employee managers",
-            "view all employees by department",
+            "view all employee by department",
             "view budget",
             "exit"]
         }
@@ -64,23 +64,26 @@ function menu() {
         if(info.option==="add employee"){
             addEmployee()
         }
-        if(info.option==="view all employees by manager"){
-            addEmployee()
+        if(info.option==="view all employee by manager"){
+            viewEmployeeManager()
         }
         if(info.option==="remove department"){
-            addEmployee()
+            removeDepartment()
         }
         if(info.option==="remove employee"){
-            addEmployee()
+            removeEmployee()
+        }
+        if(info.option==="remove role"){
+            removeRole()
         }
         if(info.option==="update employee managers"){
-            addEmployee()
+            updateEmployeeManagers()
         }
-        if(info.option==="view all employees by department"){
-            addEmployee()
+        if(info.option==="view all employee by department"){
+            viewEmployeeDepartment()
         }
         if(info.option==="view budget"){
-            addEmployee()
+           viewBudget()
         }
         
 
@@ -124,110 +127,88 @@ function menu() {
         }
 
         //add a department function goes here
-            function addDepartment() {
-                const request = "SELECT * FROM department";
-                db.query(request, function(err, res) {
-                    if (err) throw err;
-                    console.log("add a department");
-                    console.table(res);
-                    menu()
+        const addDepartment = () => {
+            inquirer
+              .prompt({
+                type: "input",
+                name: "department",
+                message: "What department would you like to add?"
+              })
+              .then((answer) => {
+                connection.query(`INSERT INTO department(role)
+                VALUES("${answer.department}")`, (err, res) => {
+                  if (err) throw err;
+                  init();
+                  menu()
                 })
-        }
+              })
+          };
         
         //add a role function goes here
-            function addRole() {
-                const request = "SELECT * FROM department";
-                db.query(request, function(err, res) {
-                    if (err) throw err;
-                    console.log("add a role");
-                    console.table(res);
-                    menu()
+        const addRole = () => {
+            inquirer
+              .prompt([
+                {
+                type: "input",
+                name: "role",
+                message: "What role would you like to add?"
+              },
+              {
+                type: "input",
+                name: "salary",
+                value: "What is their salary?"
+              },
+              {
+                type: "list",
+                name: "department",
+                value: "What department does this role belong to?",
+                choices: departments
+              }
+              ])
+              .then((answer) => {
+                connection.query(`INSERT INTO role(title, salary, department_id)
+                VALUES("${answer.role}", ${answer.salary}, ${answer.department})`, (err, res) => {
+                  if (err) throw err;
+                  init();
+                  menu()
                 })
-        }
+              })
+          };
         
       //update an employee role function goes here
-            function updateAnEmployeeRole() {
-                const request = "SELECT * FROM department";
-                db.query(request, function(err, res) {
-                    if (err) throw err;
-                    console.log("update an employee role");
-                    console.table(res);
-                    menu()
-                })
-        }
+      const updateRole = () => {
+        inquirer
+          .prompt([
+            {
+              type: "list",
+              name: "employee",
+              message: "Whose role are we updating?",
+              choices: employees,
+            },
+            {
+              type: "list",
+              name: "role",
+              message: "What is his new role?",
+              choices: roles,
+            },
+          ])
+          .then((answer) => {
+            connection.query(
+              `UPDATE employee
+        SET role_id = ${answer.role}
+        WHERE id = ${answer.employee};`,
+              (err, res) => {
+                if (err) throw err;
+                init();
+                menu()
+              }
+            );
+          });
+      };
        
         //add employee function goes here
-            function addEmployee() {
-                const request = "SELECT * FROM department";
-                db.query(request, function(err, res) {
-                    if (err) throw err;
-                    console.log("add employee");
-                    console.table(res);
-                    menu()
-                })
-        }
-        //view all employees by manager function goes here
-            function viewAllEmployeesByManager() {
-                const request = "SELECT * FROM department";
-                db.query(request, function(err, res) {
-                    if (err) throw err;
-                    console.log("View All Employees By Manager");
-                    console.table(res);
-                    menu()
-                })
-        }
-        // remove repartment function goes here
-            function removeDepartment() {
-                const request = "SELECT * FROM department";
-                db.query(request, function(err, res) {
-                    if (err) throw err;
-                    console.log("remove department");
-                    console.table(res);
-                    menu()
-                })
-        }
-        // remove employee function goes here
-            function removeEmployee() {
-                const request = "SELECT * FROM department";
-                db.query(request, function(err, res) {
-                    if (err) throw err;
-                    console.log("remove employee");
-                    console.table(res);
-                    menu()
-                })
-        }
-        // update employee managers function goes here
-            function updateEmployeeManagers() {
-                const request = "SELECT * FROM department";
-                db.query(request, function(err, res) {
-                    if (err) throw err;
-                    console.log("update employee managers");
-                    console.table(res);
-                    menu()
-                })
-        }
-        // view all employees by department function goes here
-            function viewAllEmployeesByDepartment() {
-                const request = "SELECT * FROM department";
-                db.query(request, function(err, res) {
-                    if (err) throw err;
-                    console.log("view all employees by department");
-                    console.table(res);
-                    menu()
-                })
-        }
-        // view budget function goes here
-            function viewBudget() {
-                const request = "SELECT * FROM department";
-                db.query(request, function(err, res) {
-                    if (err) throw err;
-                    console.log("view budget");
-                    console.table(res);
-                    menu()
-                })
-        }
-  
         function newEmployee() {
+            console.log('oy')
             inquirer.prompt ([
                 {
                 type: 'input',
@@ -256,36 +237,149 @@ function menu() {
                 [response.FirstName, response.LastName, response.EmployeeID, response.ManagerID]), function(err,response) {
                     if (err) throw err;
                     console.table(res);
-                    inquirer.prompt([
-                        {
-                            type: 'list',
-                            name: 'choice',
-                            message: 'select an option.',
-                            choices: [
-                                'Main Menu',
-                                'Quit'
-                            ]
+                    inquirer.prompt
+                        menu()
                             
                         }
-                      
-                    ])
-                   .then((answer) => {
-                       switch (answer.choice){
-                           case 'Main Menu':
-                               start();
-                               break;
-                               case 'Quit':
-                                   Quit();
-                       }
-                   })
-                }
-                
-            })
-        }
+                   
+                })
+                           
+        //view all employees by manager function goes here
+        const allEmployeeManagers = () => {
+            inquirer
+              .prompt({
+                type: "list",
+                name: "manager",
+                message: "choose a manager?",
+                choices: managers,
+              })
+              .then((answer) => {
+                connection.query(
+                  `SELECT first_name, last_name FROM employee
+                WHERE manager_id = ${answer.manager};`,
+                  (err, res) => {
+                    if (err) throw err;
+                    console.table(res);
+                    init();
+                    menu()
+                  }
+                );
+              });
+            }
         
-         // viewEmployees();
-         menu()
- 
+        // remove department function goes here
+        const removeDepartment = () => {
+            inquirer
+              .prompt({
+                type: "list",
+                name: "department",
+                message: "Who would you like to remove?",
+                choices: departments,
+              })
+              .then((answer) => {
+                connection.query(
+                  `DELETE FROM department WHERE id=${answer.department}`,
+                  (err, res) => {
+                    if (err) throw err;
+                    init();
+                  }
+                );
+                console.log(answer);
+                menu()
+              });
+          };
+        // remove employee function goes here
+        const removeEmployee = () => {
+            inquirer
+              .prompt({
+                type: "list",
+                name: "employee",
+                message: "Who would you like to remove?",
+                choices: employees,
+              })
+              .then((answer) => {
+                connection.query(
+                  `DELETE FROM employee WHERE id=${answer.employee}`,
+                  (err, res) => {
+                    if (err) throw err;
+                    init();
+                  }
+                );
+                console.log(answer);
+                menu()
+              });
+          };
+        const removeRole = () => {
+            inquirer
+              .prompt({
+                type: "list",
+                name: "role",
+                message: "what role would you like to remove?",
+                choices: roles,
+              })
+              .then((answer) => {
+                connection.query(
+                  `DELETE FROM roles WHERE id=${answer.role}`,
+                  (err, res) => {
+                    if (err) throw err;
+                    init();
+                  }
+                );
+                console.log(answer);
+                menu()
+              });
+          };
+        // update employee managers function goes here
+            function updateEmployeeManagers() {
+                const request = "SELECT * FROM department";
+                db.query(request, function(err, res) {
+                    if (err) throw err;
+                    console.log("update employee managers");
+                    console.table(res);
+                    menu()
+                })
+        }
+        // view all employees by department function goes here
+            //function viewAllEmployeeByDepartment() {
+                const request = "SELECT * FROM department";
+                db.query(request, function(err, res) {
+                    if (err) throw err;
+                    console.log("view all employees by department");
+                    console.table(res);
+                    menu()
+                })
+        }
+        // view budget function goes here
+        const viewBudget = () => {
+            connection.query(`SELECT salary 
+            FROM employee
+            JOIN role ON employee.role_id = role.role_id 
+            JOIN department ON role.department_id = department.department_id
+            LEFT JOIN managers on employee.manager_id = managers.manager_id`, (err, res) => {
+              if (err) throw err;
+              console.log(res);
+              var budget = [];
+              for (let i = 0; i < res.length; i++) {
+                const addBudget = res[i].salary;
+                budget.push(addBudget)
+              }
+              var sum = budget.reduce(function(a, b){
+                return a + b;
+              }, 0);
+              console.log("TOTAL BUDGET")
+              console.log(sum)
+              init();
+              
+            })
+          }
+  
+       
+        
+          function Quit() {
+            console.log('Goodbye!');
+            process.exit();
+            
+        }
       
         
   
